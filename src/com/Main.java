e2c6666624febb00221c3467ce2d443d035698f1
ivan4000 <sxln1@mail.ru>
2018-11-02 20:39:36 +0300

    JL118customDialog

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,7 +1,48 @@
 package com;
 
+import jdk.nashorn.internal.scripts.JD;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
+
     public static void main(String[] args) {
+        jFrame.add(jPanel);
+        JButton jButton=new JButton("show dialog");
+        jPanel.add(jButton);
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MyDialog myDialog=new MyDialog();
+                myDialog.setVisible(true);
+            }
+        });
+
+    }
+
+    static class MyDialog extends JDialog {
+        public MyDialog() {
+            super(jFrame, "title", true);
+            add(new JTextField(), BorderLayout.NORTH);
+            add(new JButton("button"), BorderLayout.SOUTH);
+            setBounds(500, 500, 250, 150);
+        }
+    }
+
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {
+        };
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 500, 500);
+        return jFrame;
 
     }
 }
